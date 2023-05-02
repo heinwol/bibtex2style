a42bbf65d7a86a76cefdabdbf168a0f0037153ec
heinwol <koreshkovhw@yandex.ru>
2023-05-02 13:46:33 +0300

    added poetry

--- flake.nix
+++ flake.nix
@@ -1,15 +1,53 @@
 {
-  description = "A very basic flake";
+  description = "bibtex2style is a script that takes .bib file as an input and produces an .xlsx file with entries processed by biblatex with an according style (like `gost`). It also respects bold an italics fonts!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # pypi-deps-db = {
+    #   url = "github:DavHau/pypi-deps-db";
+    #   flake = false;
+    # };
+    mach-nix = {
+      url = "github:DavHau/mach-nix/master";
+      # inputs = {
+        # nixpkgs.follows = "nixpkgs";
+        # pypi-deps-db.follows = "pypi-deps-db";
+      # };
+    };
+    # mach-nix.python = "python310";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, mach-nix }: let
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    system = "x86_64-linux";
+  
+    pkgs = nixpkgs.legacyPackages.${system};
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    pyEnv = mach-nix.lib.${system}.mkPython {
+      requirements = ''
+        PyMuPDF
+        fitz
+        toolz
+        openpyxl==3.1
+        lenses
+        glom
+      '';
+    };
+    
+    buildInputs = with pkgs; [
+      pyEnv
+    ];
 
+    devShells.default = pkgs.mkShell {
+      inherit buildInputs;
+      shellHook = ''
+        echo "lalala"
+      '';
+    };
+    
+  in {
+    devShells.${system} = devShells;
+    # packages.x86_64-linux.bibtex2style = 1;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.bibtex2style;
   };
 }
