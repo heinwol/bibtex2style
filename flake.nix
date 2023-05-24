df718a8962360a0c95ef8f689b9781a80ccaaaa2
heinwol <koreshkovhw@yandex.ru>
2023-05-24 17:08:47 +0300

    formatted flake.nix with nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -5,87 +5,90 @@ rec {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs }:
+    let
 
-    system = "x86_64-linux";
-  
-    pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
 
-    tex-with-pkgs = (pkgs.texlive.combine {
-      inherit (pkgs.texlive)
-        scheme-basic
-        biber
-        biblatex
-        biblatex-gost
-        collection-langcyrillic
-        cm-unicode
-        latexmk
-        luatex
-        luatex85
-        luatexbase
-        polyglossia
-        standalone
-        varwidth
-        ;
-    });
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    buildInputs = [
-      tex-with-pkgs
-    ];
+      tex-with-pkgs = (pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-basic
+          biber
+          biblatex
+          biblatex-gost
+          collection-langcyrillic
+          cm-unicode
+          latexmk
+          luatex
+          luatex85
+          luatexbase
+          polyglossia
+          standalone
+          varwidth
+          ;
+      });
 
-    devShells.default = pkgs.mkShell {
-      packages = buildInputs ++ [
-        pkgs.poetry
-        (pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          preferWheels = true; # else it fails
-
-          # for development;
-          # TODO: remove runtime dependency
-          extraPackages = (p: [p.python-lsp-server]);
-        })];
-      shellHook = ''
-        echo "entering dev shell..."
-        eval fish || true
-      '';
-    };
-
-    package-env = pkgs.poetry2nix.mkPoetryApplication {
-      projectDir = ./.;
-      preferWheels = true; # else it fails
-    };
-    
-    bibtex2style = pkgs.stdenvNoCC.mkDerivation {
-
-      name = "bibtex2style";
-
-      src = ./.;
-      
-      buildInputs = buildInputs ++ [
-        pkgs.makeWrapper
+      buildInputs = [
+        tex-with-pkgs
       ];
 
-      installPhase = ''
-        mkdir -p $out/bin
-        ln -s "${package-env.dependencyEnv}/bin/bibtex2style" "$out/bin/"
-      '';
-      
-      postFixup = ''
-        wrapProgram $out/bin/bibtex2style \
-          --set PATH ${pkgs.lib.makeBinPath buildInputs}
-      '';
-      
-      meta = {
-        inherit description;
+      devShells.default = pkgs.mkShell {
+        packages = buildInputs ++ [
+          pkgs.poetry
+          (pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            preferWheels = true; # else it fails
+
+            # for development;
+            # TODO: remove runtime dependency
+            extraPackages = (p: [ p.python-lsp-server ]);
+          })
+        ];
+        shellHook = ''
+          echo "entering dev shell..."
+          eval fish || true
+        '';
+      };
+
+      package-env = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        preferWheels = true; # else it fails
+      };
+
+      bibtex2style = pkgs.stdenvNoCC.mkDerivation {
+
+        name = "bibtex2style";
+
+        src = ./.;
+
+        buildInputs = buildInputs ++ [
+          pkgs.makeWrapper
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          ln -s "${package-env.dependencyEnv}/bin/bibtex2style" "$out/bin/"
+        '';
+
+        postFixup = ''
+          wrapProgram $out/bin/bibtex2style \
+            --set PATH ${pkgs.lib.makeBinPath buildInputs}
+        '';
+
+        meta = {
+          inherit description;
+        };
+      };
+
+    in
+    {
+      devShells.${system} = devShells;
+      packages.${system} = {
+        bibtex2style = bibtex2style;
+        default = bibtex2style;
+        package-env = package-env.dependencyEnv;
       };
     };
-    
-  in {
-    devShells.${system} = devShells;
-    packages.${system} = {
-      bibtex2style = bibtex2style;
-      default = bibtex2style;
-      package-env = package-env.dependencyEnv;
-    };
-  };
 }
